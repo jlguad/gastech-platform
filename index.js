872729f053d12bf426d8b85128c141d1233c595c
jlguad <jlguad@gmail.com>
2026-01-03 23:01:00 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ status: 'GasTech API running' });
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
